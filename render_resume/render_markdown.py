aa8beaa3a2e71f90bd76d00c158c2be312ff660a
Gergo N. <chege54@gmail.com>
2025-05-15 08:45:42 +0000

    Add markdown render workflow

--- render_resume/render_markdown.py
+++ render_resume/render_markdown.py
@@ -2,6 +2,19 @@ from jinja2 import Environment, FileSystemLoader
 import argparse
 from base64_encode import encode_image_to_base64
 from resume import collect_resume_data
+import os
+from pathlib import Path
+import shutil
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def get_output_folder() -> Path:
+    output_folder = Path(os.path.join(MODULE_DIR, "../output"))
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+    os.makedirs(output_folder)
+    return output_folder
+
 
 if __name__ == "__main__":
 
@@ -32,10 +45,11 @@ if __name__ == "__main__":
     resume_data = collect_resume_data(args.folder)
 
     # Jinja2 rendering
-    env = Environment(loader=FileSystemLoader('./templates/'),  trim_blocks=True, lstrip_blocks=True)
+    env = Environment(loader=FileSystemLoader(f"{MODULE_DIR}/../templates/"),  trim_blocks=True, lstrip_blocks=True)
     template = env.get_template("markdown.jinja") # TODO: add multiple templates
 
-    output = template.render(resume=resume_data, profile_image_b64=profile_image_b64)
-
-    with open(f"{args.output}", mode="w") as f:
-        f.write(output)
+    output_path = get_output_folder().joinpath(args.output)
+    with open(output_path, mode="w") as f:
+        rendered_output = template.render(resume=resume_data, profile_image_b64=profile_image_b64)
+        f.write(rendered_output)
+        print(f"Markdown resume saved to {output_path}")
